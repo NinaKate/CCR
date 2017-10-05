8ca68ea4285915bfa85e6cc7f9aabbed579fc7d8
Nina Stein <ninastei@buffalo.edu>
2017-10-05 12:13:02 -0400

    Okay so apparently this is not up to date

--- src/vdprunnet.cpp
+++ src/vdprunnet.cpp
@@ -11,18 +11,19 @@ using namespace std;
 int main(){
   unsigned int n;
   const int inc = 1;
-  for (int i=0;i<15;i++){
-    n=(pow(4.0,i));
+  for (int i=0;i<29;i++){
+    n=(pow(2.0,i));
     const int m = n;
     double *A = new double[n];
   double *B = new double[n];
   double C = 0;
   
   auto begin = std::chrono::high_resolution_clock::now();
-  C = ddot_(&m,A,&inc,B,&inc);
+  for (int j =0;j<100;j++){
+    C = ddot_(&m,A,&inc,B,&inc);}
   auto end = std::chrono::high_resolution_clock::now();
   double t = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
-  t = t*pow(10,-9);
+  t = t*pow(10,-9)/100;
   double N = 8*n;
   double flops = 2*n/t;
   flops = flops*pow(10,-6);
