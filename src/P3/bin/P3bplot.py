bd483effeb83cf7560c7d588631e0636bfeb18ea
Nina Stein <ninastei@buffalo.edu>
2017-12-10 08:42:15 -0500

    Alright, that's the P1 plot fixed

--- src/P3/bin/P3bplot.py
+++ src/P3/bin/P3bplot.py
@@ -12,18 +12,18 @@ fine = prefix+"bfine.txt"
 [x3,y3]=rp.read_plot(fine)
 
 
-fig,(ax1,ax2,ax3)=plt.subplots(3)
+fig,(ax1,ax2,ax3)=plt.subplots(3,figsize=(6,18))
 ca = 20*20
-ax1.scatter(x1,y1,s=ca,marker='s')
+ax1.scatter(x1,y1,s=ca,marker='s',lw=0.0)
 ax1.set_xlim([-2.0,0.5])
 ax1.set_ylim([-1.0,1.0])
 #ax1.set_title("Coarse grid plot, h=0.25")
 ma = 10*10
-ax2.scatter(x2,y2,s=ma,marker='s')
+ax2.scatter(x2,y2,s=ma,marker='s',lw=0.0)
 ax2.set_xlim([-2.0,0.5])
 ax2.set_ylim([-1.0,1.0])
 #ax2.set_title("Medium grid plot, h=0.05")
-ax3.scatter(x3,y3,s=0.5*0.5)
+ax3.scatter(x3,y3,s=0.75*0.75)
 #ax3.set_title("Fine grid plot, h = 0.005")
 plt.xlim([-2.0,0.5])
 plt.ylim([-1.0,1.0])
